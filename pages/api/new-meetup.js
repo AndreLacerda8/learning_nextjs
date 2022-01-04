dd8fe7a0582f35d553b029b492c42653bd2b1f0a
André Lacerda <andrlacerda180@gmail.com>
2022-01-04 16:22:08 -0300

    create method to add meetup in database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,16 +1,20 @@
 import { MongoClient } from 'mongodb'
 
-export async function handler(req, res){
-  if(req.method === 'POST'){
-    const data = req.body
-
-    const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ashrk.mongodb.net/meetups?retryWrites=true&w=majority`)
-    const db = client.db()
-
-    const meetupsCollection = db.collection('meetups')
-    const result = await meetupsCollection.insertOne(data)
-    console.log(result)
-    client.close()
-    res.status(201).json({message: 'Meetup inserted'})
+export default async function handler(req, res){
+  try{
+    if(req.method === 'POST'){
+      const data = req.body
+  
+      const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ashrk.mongodb.net/meetups?retryWrites=true&w=majority`)
+      const db = client.db()
+  
+      const meetupsCollection = db.collection('meetups')
+      const result = await meetupsCollection.insertOne(data)
+      console.log(result)
+      client.close()
+      res.status(201).json({message: 'Meetup inserted'})
+    }
+  } catch(error){
+    console.error(error)
   }
 }
