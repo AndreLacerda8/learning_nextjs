379701b5b68e9ad91fbf7ae7122c349d2cdf70e1
André Lacerda <andrlacerda180@gmail.com>
2022-01-04 14:41:21 -0300

    create page of mettup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import { MeetupDetail } from '../../components/meetups/MeetupDetail'
+
+export default function MeetupDetails(){
+  return (
+    <MeetupDetail
+      image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzuY2Si3nXBYTAsDFkpWJw4TewBh1pzcNYXw&usqp=CAU'
+      title='First meetup'
+      description='This is a first meetup'
+      address='Some Street 8, some city'
+    />
+  )
+}
