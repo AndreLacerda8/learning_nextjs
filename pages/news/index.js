8d7f4ec02e60a45f286609c5989406eb36d97164
André Lacerda <andrlacerda180@gmail.com>
2022-01-04 13:55:17 -0300

    use Link component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,21 @@
+import Link from 'next/link'
+
 export default function NewsPage(){
   return (
-    <h1>News Page</h1>
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-framework'>
+            NextJS is a framework
+          </Link>
+        </li>
+        <li>
+          <Link href='/news/something-else'>
+            Something else
+          </Link>
+        </li>
+      </ul>
+    </>
   )
 }
