7b20e9f779a2d66b1cec7362bf44c89e50979441
André Lacerda <andrlacerda180@gmail.com>
2022-01-04 15:21:27 -0300

    use getStaticProps and getStaticPaths in dynamic route meetupId

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,3 +10,37 @@ export default function MeetupDetails(){
     />
   )
 }
+
+export async function getStaticPaths(){
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1'
+        }
+      },
+      {
+        params: {
+          meetupId: 'm2'
+        }
+      }
+    ]
+  }
+}
+
+export async function getStaticProps(context){
+  const meetupId = context.params.meetupId
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzuY2Si3nXBYTAsDFkpWJw4TewBh1pzcNYXw&usqp=CAU',
+        title:'First meetup',
+        description:'This is a first meetup',
+        address:'Some Street 8, some city'
+      }
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,21 @@ export default function HomePage(props){
   )
 }
 
+// export async function getServerSideProps(context){
+//   const req = context.req
+//   const res = context.res
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
+
 export async function getStaticProps(){
   return {
     props: {
       meetups: DUMMY_MEETUPS
-    }
+    },
+    revalidate: 1
   }
 }
