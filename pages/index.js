84cdec090f00f1a12bb0b6663f78b1c54e205c22
André Lacerda <andrlacerda180@gmail.com>
2022-01-04 14:16:41 -0300

    create index

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzuY2Si3nXBYTAsDFkpWJw4TewBh1pzcNYXw&usqp=CAU',
+    address: 'Some address 5, 123 some city',
+    description: 'this is a first meetup'
+  },
+  {
+    id: 'm2',
+    title: 'A second Meetup',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqG4rU6PTybJGkJQ90iLxJFwwsTYf8A6dWBA&usqp=CAU',
+    address: 'other address 58, 1890 some city',
+    description: 'this is a second meetup'
+  }
+]
+
+export default function HomePage(){
+  return (
+    <MeetupList meetups={DUMMY_MEETUPS} />
+  )
+}
